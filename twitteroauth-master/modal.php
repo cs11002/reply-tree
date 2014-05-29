4f0c0f2533f0e7fc56e46e8a0d4bd14661c36186
k-nishioka <nishikei0116@gmail.com>
2014-05-29 16:02:15 +0900

    ツリーの表示位置修正

--- twitteroauth-master/modal.php
+++ twitteroauth-master/modal.php
@@ -1,3 +1,6 @@
 <div id="div787">
-	<img src="./images/photo1.jpg" width="50%" height="50%">
+	<center>
+		Reply-tree<br />
+		<img src="./images/photo1.jpg" width="50%" height="50%">
+	</center>
 </div>
