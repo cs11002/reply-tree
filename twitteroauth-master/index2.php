7f6738a0ded0fbafd31ad1914b1286f552c6272a
Asai <asai@sakailab.info>
2014-07-10 14:29:59 +0900

    ログアウト機能及びログイン画面でreadmore非表示化

--- twitteroauth-master/index2.php
+++ twitteroauth-master/index2.php
@@ -54,6 +54,12 @@ for ( $i=0; $i<count($json); $i++ ){
 
 }
 
+$tweet .= "<form id=\"read-more\">\n";
+$tweet .= "<center>\n";
+$tweet .= "<input class=\"btn btn-default\" id=\"read-more\" type=\"button\" value=\"readmore\" onclick=\"readmore()\">\n";
+$tweet .= "</center>\n";
+$tweet .= "</form>\n";
+
 $content = $tweet;
 
 /* Include HTML to display on the page */
