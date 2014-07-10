f12a2a8c7d61498fa46ea5b8d97e246b18d65dc6
cs11056 <cs11056@s.inf.shizuoka.ac.jp>
2014-07-10 12:56:22 +0900

    modalの表示を変更しました

--- twitteroauth-master/modal.php
+++ twitteroauth-master/modal.php
@@ -5,17 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Loading Bootstrap -->
     <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
-    <!-- Loading Flat UI -->
-    <link href="css/flat-ui.css" rel="stylesheet">
     <link href="./css/style.css" rel="stylesheet">
     <script src="./js/jquery-2.1.1.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="./js/jquery.leanModal.min.js"></script>
     <link rel="shortcut icon" href="images/favicon.ico">
+	
+	<style type="text/css">
+	<!--
+		div {
+			font-family: "Lato",Helvetica,Arial,sans-serif;
+			font-size: 18px;
+    		line-height: 1.72222;
+			color: #34495E;
+		}
+	-->
+</style>
   </head>
  <body>
 <?php session_start(); ?>
 <!--<div id="div787">-->
-	<center>
 		<b>Reply-tree</b><br />	
  	<?php
 
@@ -47,37 +55,60 @@
 			$c++;
 			$tweet[$c] = $connection->get($api);
 		}
-$tweet_user = array();
-for($i=0; $i<$c+1;$i++) {
- $tweetdata = $tweet[$i];
- if(!array_key_exists($tweetdata->user->id_str,$tweet_user)) {
-  $tweet_user[] = $tweetdata->user->id_str;
- }
-}
+
+		$tweet_user = array(); //ツイートした人を入れる配列
+		$tweet_user[0] = -1;
+		
+		$u_c = 0;
+		for($i=0; $i<$c+1;$i++) {
+			$tweetdata = $tweet[$i];
+			if($i == 0){
+				$tweet_user[$i] = $tweetdata->user->id_str;
+			}else{
+				$u_c = count($tweet_user);
+				for($j=0;$j<$u_c;$j++){
+					if(intval($tweetdata->user->id_str) == $tweet_user[$j]) {
+						break;
+					}
+					if($j == ($u_c-1)){
+						$tweet_user[$u_c] = $tweetdata->user->id_str;
+					}
+				}
+			}
+/*			echo $tweetdata->user->id_str.'-';
+			print_r($tweet_user);
+			echo'<br/>';
+*/
+			//echo $i.'-'.$tweet_user[$i].'<br>';			
+		}
 
 		
 		
 		//会話一覧をテスト表示
-$top = 20;
+	$top = 20;
 	for($i=0; $i<$c+1;$i++) {
 		for($j=0;$j<count($tweet_user);$j++){
 			if($tweet[$i]->user->id_str == $tweet_user[$j]){
-				$left = -300 + 300*($j-1);
+				$left =  150*($j);
+				break;
+		echo $j.'-'.$left.'<br>';
 			}
 		}
+		$j=0;
 		echo '
-<div style="position:relative;left:'.$left.'px; border-style: solid ; border-width: 1px; padding: 10px 5px 10px 20px; border-color: white; color: black; background-color: white; width: 400px; border-radius: 30px; box-shadow: 5px 5px 5px #AAA;">';
-		echo '<article id='.$i.'><img src="'.$tweet[$i]->user->profile_image_url.'"/>';
-		echo '<span>'.$tweet[$i]->user->name.'</span>';
-		echo '@'.$tweet[$i]->user->screen_name.'<br/>';
+<div style="font-size: 9px; position:relative;left:'.$left.'px; border-style: solid ; border-width: 1px; padding: 10px 5px 10px 20px; border-color: white; color: black; background-color: white; width: 200px; border-radius: 15px; box-shadow: 3px 3px 3px #AAA;">';
+		echo '<img src="'.$tweet[$i]->user->profile_image_url.'" width="40px" align="left"/>';
+		echo '<span style="font-size: 12px;">'.$tweet[$i]->user->name.'</span>';
+		echo '@'.$tweet[$i]->user->screen_name.'<br clear="left"/>';
 		echo $tweet[$i]->text.'<br/>';
 		echo $tweet[$i]->created_at;
 		echo'</div></br>';
+
+
 		$top = $top + 300;
 	}
   	?>
  
-	</center>
 <!--</div>-->
  </body>
  </html>
